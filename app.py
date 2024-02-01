b4071319fb4098183df7bdd85b6dbb49286187da
zajko99 <99417640+zajko99@users.noreply.github.com>
2024-02-01 22:51:08 +0000

    updated app.py and home.html

--- app.py
+++ app.py
@@ -3,10 +3,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+JOBS = [{'id':1,
+       'title': 'Luxury beachfront',
+       'location': 'Honolulu',
+       'price':"$100000"},
+       {'id':2,
+          'title': 'Luxury apt',
+          'location': 'Hono Island',
+          'price':"$32321"},
+       {'id':3,
+          'title': 'Luxury villa',
+          'location': 'Holalola',
+          'price':"$99999"}]
+
 @app.route("/")
 
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS, company_name="encering")
 
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug = True) 
